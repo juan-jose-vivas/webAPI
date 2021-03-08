53db5da40e56fa01322231c2c778c1aacc4bba11
juan-jose-vivas <67050527+juan-jose-vivas@users.noreply.github.com>
2021-03-08 09:55:00 +0100

    Create Persona.cs

---
+++ conexion_sin BBDD/Persona.cs
@@ -0,0 +1,11 @@
+using System;
+namespace web_api_core_ServicioAPI.Models
+{
+    public class Persona
+    {
+        public int IdPersona { get; set; }
+        public String Nombre { get; set; }
+        public String Email { get; set; }
+        public int Edad { get; set; }
+    }
+}
