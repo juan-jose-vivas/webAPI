b17aacf0aae6acdfc852bc2c34654387ed9caf1e
juan-jose-vivas <67050527+juan-jose-vivas@users.noreply.github.com>
2021-03-08 13:02:01 +0100

    Create Empleado.cs

---
+++ conexion_sin_BBDD2/Empleado.cs
@@ -0,0 +1,20 @@
+using System;
+namespace web_api_core_ServicioAPI2
+{
+    public class Empleado
+    {
+
+        public int EMPNO { get; set; }
+        public String APELLIDO { get; set; }
+        public String OFICIO { get; set; }
+        public int DIR { get; set; }
+        public DateTime FECHA_ALTA { get; set; }
+        public int SALARIO { get; set; }
+        public int COMISION { get; set; }
+        public int DEPT_NO { get; set; }
+
+    }
+
+
+
+}
